44195cc54493feb3dbea52ea68d814cac7e18b66
Pierre Carrier <pc@rrier.fr>
2025-08-31 19:08:21 +0000

    nix

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      flake-utils,
+      nixpkgs,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [ ruby ];
+        };
+
+        packages.default = pkgs.stdenv.mkDerivation {
+          name = "baze";
+          version = "1.1.1";
+          src = ./.;
+
+          buildInputs = [ pkgs.ruby ];
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp -r bin/* $out/bin/
+            chmod +x $out/bin/*
+          '';
+        };
+      }
+    );
+}
